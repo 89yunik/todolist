3477c02d35c0e8ce6a7d3cf2c1c7aa30c2a2ee57
Nick Yoon <89yunik@gmail.com>
2024-08-24 16:47:25 +0900

    fix : addTodo/removeTodo change state

--- shared/todoLogic.ts
+++ shared/todoLogic.ts
@@ -1,9 +1,11 @@
-export const addTodo = (todos: Map<string, string>, id: string, text: string): Map<string, string> => {
-  todos.set(id, text)
-  return todos
+export const addTodo = (prevTodos: Map<string, string>, id: string, input: string): Map<string, string> => {
+  const newTodos = new Map(prevTodos)
+  newTodos.set(id, input)
+  return newTodos
 }
 
-export const removeTodo = (todos: Map<string, string>, id: string): Map<string, string> => {
-  todos.delete(id)
-  return todos
+export const removeTodo = (prevTodos: Map<string, string>, id: string): Map<string, string> => {
+  const newTodos = new Map(prevTodos)
+  newTodos.delete(id)
+  return newTodos
 }
