17278f780bdcc30a90b374b22abb8f656897ce4c
Nick Yoon <89yunik@gmail.com>
2024-08-26 22:28:15 +0900

    feat : handleEditTodo fix : handleAddTodo style : renameClasses

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,13 +5,27 @@ import { addTodo, removeTodo } from "@/../../shared/todoLogic"
 
 export default function Home() {
   const [todos, setTodos] = useState<Map<string, string>>(new Map())
-  const [input, setInput] = useState<string>("")
+  const [addTodoInput, setAddTodoInput] = useState<string>("")
+  const [updateTodoInput, setUpdateTodoInput] = useState<string>("")
+  const [editId, setEditId] = useState<string>("")
 
-  const handleAddTodo = () => {
-    if (input.trim() === "") return
-    const id = Date.now().toString()
-    setTodos(addTodo(todos, id, input))
-    setInput("")
+  const handleAddTodo = (todoText: string, id: string = "") => {
+    if (todoText.trim() === "") return
+    if (!id) {
+      id = Date.now().toString()
+      setAddTodoInput("")
+    }
+    setTodos(addTodo(todos, id, todoText))
+  }
+
+  const handleEditTodo = (id: string, rawText: string, updatedText: string) => {
+    if (!editId) {
+      setEditId(id)
+      setUpdateTodoInput(rawText)
+    } else {
+      setEditId("")
+      handleAddTodo(updatedText, id)
+    }
   }
 
   const handleRemoveTodo = (id: string) => {
@@ -22,16 +36,19 @@ export default function Home() {
     <div id="app-container">
       <h1 id="app-name">Todo List</h1>
       <div id="add-todo-container">
-        <input id="todo-list-input" type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Add a new todo..." />
-        <button id="todo-add-button" onClick={handleAddTodo}>
+        <input id="add-todo-input" className="todo-input" type="text" value={addTodoInput} onChange={(e) => setAddTodoInput(e.target.value)} placeholder="Add a new todo..." />
+        <button className="todo-crud-button" onClick={() => handleAddTodo(addTodoInput)}>
           Add Todo
         </button>
       </div>
       <ul id="todo-list">
         {Array.from(todos).map(([todoId, todoText]) => (
           <li key={todoId} className="todo-item">
-            <span className="todo-text">{todoText}</span>
-            <button className="todo-remove-button" onClick={() => handleRemoveTodo(todoId)}>
+            {editId == todoId ? <input className="todo-input" value={updateTodoInput} onChange={(e) => setUpdateTodoInput(e.target.value)}></input> : <span className="todo-text">{todoText}</span>}
+            <button className="todo-crud-button" onClick={() => handleEditTodo(todoId, todoText, updateTodoInput)}>
+              Update
+            </button>
+            <button className="todo-crud-button" onClick={() => handleRemoveTodo(todoId)}>
               Delete
             </button>
           </li>
