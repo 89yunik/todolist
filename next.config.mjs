ee8a13e019400a0fe3d984a57e51c587d5b9af28
Nick Yoon <89yunik@gmail.com>
2024-08-31 13:44:55 +0900

    fix : change path before deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/todolist",
+  assetPrefix: "/todolist/",
+}
 
-export default nextConfig;
+export default nextConfig
